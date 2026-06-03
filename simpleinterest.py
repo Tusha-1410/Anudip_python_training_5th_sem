5df37ce1ef879e10625737ec382c4f96caffec6e
Tusha-1410 <a2024cseaiml11555@imsec.ac.in>
2026-06-03 15:05:08 +0530

    Update simpleinterest.py

--- simpleinterest.py
+++ simpleinterest.py
@@ -1,7 +1,14 @@
-p = float(input("Enter principal: "))
-r = float(input("Enter rate: "))
-t = float(input("Enter time: "))
 
-si = (p * r * t) / 100
+principal = float(input("Enter principal: "))
+if principal <= 0:
+        print("Invalid Principal Amount!")
+        exit()
+rate = float(input("Enter rate (%): "))
+if rate <= 0:
+        print("Invalid Rate of Interest!")
+time = int(input("Enter time (years): "))
+if time <= 0:
+        print("Invalid Time!")
 
-print("Simple Interest =", si)
+simple_interest = (principal * rate * time) / 100
+print("Simple Interest =", simple_interest)
