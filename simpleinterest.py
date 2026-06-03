52819bee9bb65673461d5bb2d7674dbfafcbe88d
Tusha-1410 <a2024cseaiml11555@imsec.ac.in>
2026-06-03 15:10:39 +0530

    Update simpleinterest.py

--- simpleinterest.py
+++ simpleinterest.py
@@ -6,9 +6,11 @@ if principal <= 0:
 rate = float(input("Enter rate (%): "))
 if rate <= 0:
         print("Invalid Rate of Interest!")
+        exit()
 time = int(input("Enter time (years): "))
 if time <= 0:
         print("Invalid Time!")
+        exit()
 
 simple_interest = (principal * rate * time) / 100
 print("Simple Interest =", simple_interest)
